2b228bf34ec359362c04c82016a11d98473d1a3e
anaakaaro <aanaakaarolina@gmail.com>
2025-06-01 17:47:12 -0300

    correção de erros de português

--- src/PositivoNegativo.java
+++ src/PositivoNegativo.java
@@ -6,7 +6,7 @@ public class PositivoNegativo {
         int a;
         
         System.out.println("Digite um número:");
-        System.out.println("Se o número for maior que 0 o programa retornará P");
+        System.out.println("Se o número for maior que 0 o programa retorna P");
         System.out.println("Se o número for menor ou igual a 0 o programa retorna N");
         a = scanner.nextInt();
         verificarNumero(a);
